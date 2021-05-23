46f49faeac67e9d2c2ba03f26ff13bb5c7f6811c
Ben <pauleybj@gmail.com>
2021-05-23 14:22:52 +0100

    implemented socket.io save notification

---
+++ client/src/components/UpdateMessage/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+import { Message, Transition } from "semantic-ui-react";
+
+const UpdateMessage = (props) => {
+  const { isVisible, favorite } = props.savedUpdate;
+  return (
+    <Transition.Group animation="fade" duration={500}>
+      {isVisible && (
+        <div>
+          <Message
+            className="update-message"
+            positive
+            icon="heart"
+            header="Book saved:"
+            content={`${favorite.title} by ${favorite.authors}`}
+          />
+        </div>
+      )}
+    </Transition.Group>
+  );
+};
+
+export default UpdateMessage;
